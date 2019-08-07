40a386a7ad3d7002b0cabd5405bfc4741fb23815
Thiago Soares Mota <thiagomota456@outlook.com>
2019-08-07 17:24:08 -0300

    Add projeto ao repositório

---
+++ TestandoUsoDONlog/Program.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestandoUsoDONlog
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            LogManager.Configuration = config;
+
+            int i = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Logger.Info(i++);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Goodbye cruel world");
+                    break;
+                }
+            }
+
+            NLog.LogManager.Shutdown();
+        }
+    }
+}
